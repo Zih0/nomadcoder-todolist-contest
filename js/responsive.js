2d1ad605e904c6ecc68d4ca0ba485a4de566837d
ziho <ziho@kakao.com>
2021-02-06 17:35:47 +0900

    Auto stash before merge of "gh-pages" and "origin/gh-pages"

--- js/responsive.js
+++ js/responsive.js
@@ -1,12 +1,57 @@
-const chevBar = document.querySelector('.chev-bar');
-
+const chevBar = document.querySelector(".chev-bar");
+let zidx = 0;
 function handleChevIcon() {
-	const icon = chevBar.querySelector('i');
-	if (icon.classList.contains('fa-chevron-down')) {
-		icon.classList.replace('fa-chevron-down', 'fa-chevron-right');
-		toDoList.style.display = 'none';
+	const icon = chevBar.querySelector("i");
+	if (icon.classList.contains("fa-chevron-down")) {
+		icon.classList.replace("fa-chevron-down", "fa-chevron-right");
+		toDoList.style.display = "none";
 	} else {
-		icon.classList.replace('fa-chevron-right', 'fa-chevron-down');
-		toDoList.style.display = 'block';
+		icon.classList.replace("fa-chevron-right", "fa-chevron-down");
+		toDoList.style.display = "block";
 	}
+}
+
+// target elements with the "draggable" class
+interact(".draggable").draggable({
+	// enable inertial throwing
+	inertia: true,
+	// keep the element within the area of it's parent
+	modifiers: [
+		interact.modifiers.restrictRect({
+			restriction: "parent",
+			endOnly: true,
+		}),
+	],
+	// enable autoScroll
+	autoScroll: true,
+
+	listeners: {
+		// call this function on every dragmove event
+		move: dragMoveListener,
+
+		// call this function on every dragend event
+	},
+});
+
+function dragMoveListener(event) {
+	var target = event.target;
+	// keep the dragged position in the data-x/data-y attributes
+	zidx += 1;
+	target.style.zIndex = zidx;
+	var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+	var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+	// translate the element
+	target.style.webkitTransform = target.style.transform = "translate(" + x + "px, " + y + "px)";
+
+	// update the posiion attributes
+	target.setAttribute("data-x", x);
+	target.setAttribute("data-y", y);
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
+
+function getfocus() {
+	document.querySelector(".input").focus();
 }
--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,15 @@
-const toDoForm = document.querySelector('.js-toDoForm'),
-	toDoInput = toDoForm.querySelector('input'),
-	toDoList = document.querySelector('.js-toDoList'),
-	prevCommand = document.querySelector('.prevcommand');
+const toDoForm = document.querySelector(".js-toDoForm"),
+	toDoInput = toDoForm.querySelector("input"),
+	toDoList = document.querySelector(".js-toDoList"),
+	prevCommand = document.querySelector(".prevcommand");
 
-const TODOS_LS = 'toDos';
+const TODOS_LS = "toDos";
 
 let toDos = [];
 
 function deleteToDo(event) {
-	if (typeof event === 'string') {
-		const spans = toDoList.querySelectorAll('li span');
+	if (typeof event === "string") {
+		const spans = toDoList.querySelectorAll("li span");
 		const spansArr = Array.from(spans);
 		spansArr.every((li) => {
 			if (li.innerHTML === event) {
@@ -50,63 +50,63 @@ function saveToDos() {
 function handleSubmit(event) {
 	event.preventDefault();
 	const currentValue = toDoInput.value;
-	const command = currentValue.split(' ', 1);
-	let toDo = currentValue.replace(command, '');
+	const command = currentValue.split(" ", 1);
+	let toDo = currentValue.replace(command, "");
 	paintPrevCommand(currentValue);
-	if (command[0] === 'touch') {
+	if (command[0] === "touch") {
 		console.log(toDo);
 		paintToDo(toDo);
-	} else if (command[0] === 'rm') {
+	} else if (command[0] === "rm") {
 		deleteToDo(toDo);
-	} else if (command[0] === 'clear') {
+	} else if (command[0] === "clear") {
 		while (prevCommand.firstChild) {
 			prevCommand.removeChild(prevCommand.firstChild);
 		}
-	} else if (currentValue === '') {
+	} else if (currentValue === "") {
 	} else {
 		paintPrevCommand(`command not found: ${command[0]}`);
 	}
-	toDoInput.value = '';
+	toDoInput.value = "";
 }
 
 function paintPrevCommand(currentValue) {
-	const path = document.createElement('div');
-	path.className = 'path';
-	const box = document.createElement('div');
-	box.className = 'path-box';
-	const commander = document.createElement('span');
-	commander.innerText = '~/toDo>';
+	const path = document.createElement("div");
+	path.className = "path";
+	const box = document.createElement("div");
+	box.className = "path-box";
+	const commander = document.createElement("span");
+	commander.innerText = "~/toDo>";
 	box.appendChild(commander);
 	path.appendChild(box);
-	const todospan = document.createElement('span');
+	const todospan = document.createElement("span");
 	todospan.innerText = currentValue;
 	path.appendChild(todospan);
 	prevCommand.appendChild(path);
 }
 
 function findIcon(todo) {
-	const icon = document.createElement('i');
-	const extension = todo.split('.')[1];
-	if (extension === 'html') {
-		icon.classList.add('fab', 'fa-html5');
-	} else if (extension === 'css') {
-		icon.classList.add('fab', 'fa-css3');
-	} else if (extension === 'js') {
-		icon.classList.add('fab', 'fa-js');
+	const icon = document.createElement("i");
+	const extension = todo.split(".")[1];
+	if (extension === "html") {
+		icon.classList.add("fab", "fa-html5");
+	} else if (extension === "css") {
+		icon.classList.add("fab", "fa-css3");
+	} else if (extension === "js") {
+		icon.classList.add("fab", "fa-js");
 	}
 	return icon;
 }
 
 function paintToDo(todo) {
-	const li = document.createElement('li');
-	const box = document.createElement('div');
-	const deleteButton = document.createElement('button');
-	const span = document.createElement('span');
+	const li = document.createElement("li");
+	const box = document.createElement("div");
+	const deleteButton = document.createElement("button");
+	const span = document.createElement("span");
 	const newId = Date.now();
 	const icon = findIcon(todo);
 	span.innerText = todo;
-	deleteButton.innerText = '❌';
-	deleteButton.addEventListener('click', deleteToDo);
+	deleteButton.innerText = "❌";
+	deleteButton.addEventListener("click", deleteToDo);
 	box.appendChild(icon);
 	box.appendChild(span);
 	li.appendChild(box);
@@ -120,15 +120,10 @@ function paintToDo(todo) {
 	toDos.push(toDoObj);
 	saveToDos();
 }
-
-function getfocus() {
-	document.querySelector('.input').focus();
-}
-
 function init() {
 	loadToDos();
 
-	toDoForm.addEventListener('submit', handleSubmit);
+	toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
